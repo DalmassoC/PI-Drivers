71dc1ba43153067bdeae4507b9dd05a3ecc9e511
Candela <Dalmassoc29@gmail.com>
2023-12-05 15:25:42 -0300

    Update index.js

--- cr-pi-drivers-main/server/src/routes/index.js
+++ cr-pi-drivers-main/server/src/routes/index.js
@@ -4,21 +4,20 @@ const  {getDrivers}  = require("../controllers/getDrivers");
 const {getDriverById} = require("../controllers/getDriverById"); 
 const { getDriverByName } = require("../controllers/getDriverByName");
 const { postDriver } = require("../controllers/postDriver");
-const { deleteDriver } = require("../controllers/deleteDriver");
-const router = Router();
+const { deleteDriver } = require("../controllers/deleteDriver"); //Se importan los módulos necesarios de Express (Router) y los controladores que manejan las lógicas específicas de cada endpoint.
+const router = Router(); //Se crea un objeto de router usando el método Router proporcionado por Express.
 
-router.get("/drivers", getDrivers)
+router.get("/drivers", getDrivers) //Define una ruta de tipo GET ("/drivers") que invocará la función getDrivers del controlador cuando se realice una solicitud a esa ruta.
 
 router.get("/drivers/name/", getDriverByName)
 
-router.get("/drivers/:idDriver", getDriverById)
+router.get("/drivers/:idDriver", getDriverById) // El :idDriver indica un parámetro de ruta, que puede ser accedido desde req.params.idDriver en el controlador.
 
 router.post("/drivers", postDriver)
 
 router.delete("/drivers/:idDriver", deleteDriver)
 
-
 router.get("/teams", getTeams)
 
-module.exports = router;
+module.exports = router; //Exporta el objeto de router para que pueda ser utilizado en otros archivos, generalmente en el archivo principal de la aplicación donde se configura y se inicia el servidor.
 
