0465ec4a6f6df4cb9c21a50312a6a1951e0505fa
Candela <Dalmassoc29@gmail.com>
2023-12-06 23:12:32 -0300

    Front

--- cr-pi-drivers-main/client/src/App.jsx
+++ cr-pi-drivers-main/client/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {Route, Routes} from "react-router-dom"
+import LandingPage from './components/LandingPage/LandingPage'
+import Home from './components/Home/Home'
+import Detail from './components/Detail/Detail'
+import CrearDriver from './components/Form/Form'
+import  Home1  from './components/Home1/Home1'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = ()=> {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+
+ <Routes>
+    <Route path="/" element={<LandingPage/>}/>
+    <Route path="/home" element={<Home/>}/>
+    <Route path="/driver/:id" element={<Detail/>}/>
+    <Route path="/crearDriver" element={<CrearDriver/>}/>
+
+ </Routes>
+    </div>
   )
 }
 
